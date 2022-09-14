24be71424fbf3f469ef66c958ad27d6a57e36be5
Kiryl Pryhozhy <kirill19962003@gmail.com>
2022-09-14 18:52:01 +0300

    added lab3

---
+++ _153504_Pryhozhy_Lab3/Entities/Passenger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _153504_Pryhozhy_Lab3.Entities
+{
+    public class Passenger
+    {
+        private string idData = "Kirya";
+        private List<Tariff> purchasedTickets = new List<Tariff>();
+
+        public Passenger(string idData)
+        {
+            this.idData = idData;
+        }
+
+        public string Id
+        {
+            get => idData;
+        }
+
+        public List<Tariff> PurchasedTickets
+        {
+            get => purchasedTickets;
+        }
+
+        public double GetPriceOfPurchasedTickets()
+        {
+            return purchasedTickets.Sum(x => x.Price);
+        }
+
+        public void BuyTicket(Tariff ticket)
+        {
+            purchasedTickets.Add(ticket);
+        }
+    }
+}
---
+++ _153504_Pryhozhy_Lab3/Program.cs
@@ -0,0 +1,52 @@
+﻿using _153504_Pryhozhy_Lab3.Entities;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Station station = new Station();
+            Journal journal = new Journal();
+            //link handlers
+            station.AddPurchaseHandler((string message) => Console.WriteLine(message));
+            station.AddModificationsHandler(journal.SaveEventInfo);
+
+            station.AddTariff(10, "Minsk");
+            station.AddTariff(20, "Gomel");
+            station.AddTariff(5, "Volkovysk");
+
+            station.AddPassenger("Kirya");
+            station.AddPassenger("Igor");
+
+            station.BuyTicketToPassenger("Kirya", "Minsk");
+            station.BuyTicketToPassenger("Kirya", "Gomel");
+            station.BuyTicketToPassenger("Igor", "Gomel");
+            var count = 5;
+            //aggregate
+            Console.WriteLine($"Amount of passengers who paid more than {count}:" +
+                $" {station.GetAmountOfPassesWhoPaidMoreThan(5)}");
+
+            //group by
+            var list = station.GetSumListForEachPath("Kirya");
+            Console.WriteLine("Kirya bought next tickets:");
+            foreach(var item in list)
+            {
+                Console.WriteLine(item.Key);
+                foreach(var price in item)
+                {
+                    Console.WriteLine(price);
+                }
+                Console.WriteLine();
+            }
+            //max by
+            Console.WriteLine($"Id of passenger who paid max sum : {station.GetIdOfPassWhoPaidMaxSum()}");
+            //order by
+            Console.WriteLine("Tariffs in order by price:");
+            station.GetAllTariffsSortedByPrice().ForEach(m => Console.WriteLine(m));
+            //sum
+            Console.WriteLine($"Price of all purchased tickets: {station.PriceOfAllPurchasedTickets()}");
+            journal.PrintEventsInfo();
+        }
+    }
+}
